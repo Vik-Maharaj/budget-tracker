d2e322339e6fd5fb16e238fef0b6524ae54056d0
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-02 18:28:49 -0400

    Add status check function to idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,3 +7,14 @@ request.onupgradeneeded = function (event) {
   db.createObjectStore("new_budget", { autoIncrement: true });
 };
 
+// check to see if the app is online, if so, then runs uploadBudget()
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+request.onerror = function (event) {
+  console.log(event.target.errorcode);
+};
+
