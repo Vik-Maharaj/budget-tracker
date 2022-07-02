97942682b6f0fc6d432b945584bae28f7822023a
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-02 18:38:32 -0400

    Add save record function idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,10 +1,9 @@
 let db;
-
 const request = indexedDB.open("budget_tracker", 1);
 
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
-  db.createObjectStore("new_budget", { autoIncrement: true });
+  db.createObjectStore("pending_object", { autoIncrement: true });
 };
 
 // check to see if the app is online, if so, then runs uploadBudget()
@@ -18,3 +17,12 @@ request.onerror = function (event) {
   console.log(event.target.errorcode);
 };
 
+// if there's no connection, the run saveRecord()
+function saveRecord(record) {
+
+  const transaction = db.transaction(["pending_object"], "readwrite");
+
+  const transactionObjectStore = transaction.objectStore("pending_objectt");
+
+  transactionObjectStore.add(record);
+}
