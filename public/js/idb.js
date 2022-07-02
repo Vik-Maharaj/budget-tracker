e10e143b44934150d3c3462bb7bb2927eb7a4971
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-02 18:52:02 -0400

    Add uploadObject function idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,11 +6,11 @@ request.onupgradeneeded = function (event) {
   db.createObjectStore("pending_object", { autoIncrement: true });
 };
 
-// check to see if the app is online, if so, then runs uploadBudget()
+// check to see if the app is online, if so, then runs uploadObject()
 request.onsuccess = function (event) {
   db = event.target.result;
   if (navigator.onLine) {
-    uploadBudget();
+    uploadObject();
   }
 };
 request.onerror = function (event) {
@@ -22,7 +22,47 @@ function saveRecord(record) {
 
   const transaction = db.transaction(["pending_object"], "readwrite");
 
-  const transactionObjectStore = transaction.objectStore("pending_objectt");
+  const transactionObjectStore = transaction.objectStore("pending_object");
 
   transactionObjectStore.add(record);
 }
+
+const uploadObject = () => {
+
+    const transaction = db.transaction(["pending_object"], "readwrite");
+
+    const transactionObjectStore = transaction.objectStore("pending_object")
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                            'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+
+                const transaction = db.transaction(["pending_object"], 'readwrite');
+
+                const transactionObjectStore = transaction.objectStore("pending_object");
+
+                transactionObjectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+              });
+        }
+    }
+}
+
+// listens for a connection
+window.addEventListener('online', uploadObject);
