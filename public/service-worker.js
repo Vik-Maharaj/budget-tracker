cea434fbb430125fd17752dcf1dd48dc7a9d5b07
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-02 22:42:23 -0400

    Update service worker js

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+]
+
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
